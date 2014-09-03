5909efbb91dbf12e713d8cc47dfb13c2e3a5b16e
Sergey Konoplev <sergey.konoplev@nitropdf.com>
2014-09-03 11:59:41 -0700

    Added Rakefile to the ruby-mode list.

--- ruby.el
+++ ruby.el
@@ -2,6 +2,8 @@
 (add-hook 'ruby-mode-hook 'whitespace-mode)
 (add-hook 'ruby-mode-hook 'subword-mode)
 
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+
 ;; Indentation settings
 (add-hook 'ruby-mode-hook
 	  '(lambda ()
