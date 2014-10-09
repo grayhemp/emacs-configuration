8b8a739cb09473eb317e33073d1773816563c11e
Sergey Konoplev <gray.ru@gmail.com>
2014-10-08 18:59:40 -0700

    Added auto-fill and flyspell as minor modes to markdown-mode.

--- markdown.el
+++ markdown.el
@@ -1,3 +1,7 @@
+; Minor modes
+(add-hook 'markdown-mode-hook 'auto-fill-mode)
+(add-hook 'markdown-mode-hook 'flyspell-mode)
+
 ;; Use markdown-mode for specific file types
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
