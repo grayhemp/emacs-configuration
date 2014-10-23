590f6226b02567ba566a01a3a193b5ee25b94c90
Sergey Konoplev <gray.ru@gmail.com>
2014-10-22 18:44:40 -0700

    Fixed some identation settings.

--- markdown.el
+++ markdown.el
@@ -2,6 +2,12 @@
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 
+;; Indentation settings
+(add-hook 'markdown-mode-hook
+	  '(lambda ()
+	     (setq indent-tabs-mode nil
+		   tab-width 4)))
+
 ;; Use markdown-mode for specific file types
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
