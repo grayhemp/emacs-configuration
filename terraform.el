523acfebc9d007e022015dc9ae01fe912f34f79f
Sergey Konoplev <sergey.konoplev@whitehatsec.com>
2019-04-05 17:44:49 -0700

    Adjusted indentation in teraform mode

--- terraform.el
+++ terraform.el
@@ -1,6 +1,13 @@
 ;; Major mode
 (require 'terraform-mode)
 
+
+;; Indentation settings
+(add-hook 'terraform-mode-hook
+	  '(lambda ()
+	     (setq indent-tabs-mode nil
+		   tab-width 2)))
+
 ;; Minor modes
 (add-hook 'terraform-mode-hook 'whitespace-mode)
 (add-hook 'terraform-mode-hook 'subword-mode)
